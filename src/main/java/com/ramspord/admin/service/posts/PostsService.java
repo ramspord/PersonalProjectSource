e92a2655322cff45bfdcb917c3bf1db285921d18
zwzw9 <zwzw94@gmail.com>
2022-05-14 13:27:45 +0900

    insert update

---
+++ src/main/java/com/ramspord/admin/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.ramspord.admin.service.posts;
+
+import com.ramspord.admin.domain.PostsRepository;
+import com.ramspord.admin.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
